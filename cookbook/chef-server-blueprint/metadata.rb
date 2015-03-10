4f9c364c048101c18dddd40aebb135ea241d784c
Richard Shade <rshade@rightscale.com>
2015-03-10 13:19:10 -0500

    fixing metadata

--- cookbook/chef-server-blueprint/metadata.rb
+++ cookbook/chef-server-blueprint/metadata.rb
@@ -7,6 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.7'
 
 depends "marker"
+depends "packagecloud"
 depends "chef-server"
 
 # Support everything the chef-server cookbook supports
